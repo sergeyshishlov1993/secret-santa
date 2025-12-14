5522579e0b2946a17e27c97c793e893696c64332
sergey shishlov <shishlovsergey040593@gmail.com>
2025-12-15 00:28:11 +0200

    fix bag

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,104 @@
+<!--<script setup lang="ts">-->
+<!--import bgDesktop from './assets/1.png'-->
+<!--import bgMobile from './assets/2.png'-->
+<!--import favIcon from './assets/logo2.png'-->
+
+<!--document.title = 'Secret Santa'-->
+
+<!--const link = document.querySelector("link[rel~='icon']") as HTMLLinkElement || document.createElement('link')-->
+<!--link.rel = 'icon'-->
+<!--link.href = favIcon-->
+<!--document.head.appendChild(link)-->
+<!--</script>-->
+
+<!--<template>-->
+<!--  <div-->
+<!--      class="app-layout"-->
+<!--      :style="{-->
+<!--      '&#45;&#45;bg-desktop': `url(${bgDesktop})`,-->
+<!--      '&#45;&#45;bg-mobile': `url(${bgMobile})`-->
+<!--    }"-->
+<!--  >-->
+<!--    <div class="overlay"></div>-->
+<!--    <div class="content">-->
+<!--      <router-view />-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<style>-->
+
+<!--@import url('https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,400;0,700;1,400&display=swap');-->
+
+<!--:root {-->
+<!--  &#45;&#45;red: #D42426;-->
+<!--  &#45;&#45;green: #165B33;-->
+<!--  &#45;&#45;gold: #F8B229;-->
+<!--  &#45;&#45;white: #F8F8F8;-->
+<!--}-->
+
+<!--body {-->
+<!--  margin: 0;-->
+<!--  background-color: var(&#45;&#45;red);-->
+<!--  /* Применяем Comic Neue */-->
+<!--  font-family: 'Comic Neue', cursive, sans-serif;-->
+<!--  font-size: 20px; /* Этот шрифт лучше смотрится крупным */-->
+<!--  overflow-x: hidden;-->
+<!--  cursor: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><text y="20" font-size="20">🍭</text></svg>'), auto;-->
+<!--}-->
+
+<!--input, button {-->
+<!--  font-family: 'Comic Neue', cursive, sans-serif !important;-->
+<!--  font-weight: 700; /* Жирный для четкости */-->
+<!--  font-size: 1.5rem;-->
+<!--  letter-spacing: 0.5px;-->
+<!--}-->
+
+<!--/* Заголовки */-->
+<!--h1 { font-size: 3.5rem !important; font-weight: 700 !important; }-->
+<!--h2 { font-size: 3rem !important; font-weight: 700 !important; }-->
+<!--</style>-->
+
+<!--<style scoped>-->
+<!--.app-layout {-->
+<!--  min-height: 100vh;-->
+<!--  width: 100%;-->
+<!--  position: relative;-->
+<!--  display: flex;-->
+<!--  align-items: center;-->
+<!--  justify-content: center;-->
+<!--  background-repeat: no-repeat;-->
+<!--  background-position: center center;-->
+<!--  background-size: cover;-->
+<!--  background-attachment: fixed;-->
+<!--  background-image: var(&#45;&#45;bg-desktop);-->
+<!--}-->
+
+<!--.overlay {-->
+<!--  position: absolute;-->
+<!--  top: 0;-->
+<!--  left: 0;-->
+<!--  width: 100%;-->
+<!--  height: 100%;-->
+<!--  background: rgba(0, 0, 0, 0.4);-->
+<!--  z-index: 1;-->
+<!--}-->
+
+<!--.content {-->
+<!--  position: relative;-->
+<!--  z-index: 10;-->
+<!--  width: 100%;-->
+<!--  max-width: 400px;-->
+<!--  padding: 20px;-->
+<!--}-->
+
+<!--@media (max-width: 768px) {-->
+<!--  .app-layout {-->
+<!--    background-image: var(&#45;&#45;bg-mobile);-->
+<!--  }-->
+<!--}-->
+<!--</style>-->
+
 
 
 <script setup lang="ts">
@@ -29,7 +130,8 @@ document.head.appendChild(link)
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Marck+Script&display=swap');
+
+@import url('https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,400;0,700;1,400&family=Mountains+of+Christmas:wght@400;700&display=swap');
 
 :root {
   --red: #D42426;
@@ -41,14 +143,32 @@ document.head.appendChild(link)
 body {
   margin: 0;
   background-color: var(--red);
-  font-family: 'Marck Script', cursive, sans-serif;
+  font-family: 'Comic Neue', cursive, sans-serif;
+  font-size: 20px;
   overflow-x: hidden;
   cursor: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><text y="20" font-size="20">🍭</text></svg>'), auto;
 }
 
+
 input, button {
-  font-family: 'Marck Script', cursive, sans-serif !important;
-  font-size: 1.3rem;
+  font-family: 'Comic Neue', cursive, sans-serif !important;
+  font-weight: 700;
+  font-size: 1.5rem;
+  letter-spacing: 0.5px;
+}
+
+
+h1 {
+  font-family: 'Mountains of Christmas', cursive, serif !important;
+  font-weight: 700 !important;
+  font-size: 3.5rem !important;
+  letter-spacing: 2px;
+}
+
+h2 {
+  font-family: 'Comic Neue', cursive, sans-serif !important;
+  font-weight: 700 !important;
+  font-size: 3rem !important;
 }
 </style>
 
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,266 +1,3 @@
-<!--<script setup lang="ts">-->
-<!--import { ref } from 'vue'-->
-<!--import { collection, query, where, getDocs, runTransaction, doc } from 'firebase/firestore'-->
-<!--import { db } from '../firebase'-->
-<!--import logoImg from '../assets/logo.png'-->
-<!--import logoImg2 from '../assets/logo2.png'-->
-
-<!--interface User {-->
-<!--  id: string-->
-<!--  name: string-->
-<!--  taken: boolean-->
-<!--  santaId?: string-->
-<!--}-->
-
-<!--const name = ref('')-->
-<!--const target = ref<string | null>(null)-->
-<!--const error = ref('')-->
-<!--const loading = ref(false)-->
-
-
-<!--const getNormalizedSurname = (fullName: string) => {-->
-<!--  const surname = fullName.trim().split(/\s+/).pop()?.toLowerCase() || ''-->
-<!--  return surname.replace(/(ая|яя|ый|ий|ой|а|я)$/, '')-->
-<!--}-->
-
-<!--const getSanta = async () => {-->
-<!--  const inputName = name.value.trim()-->
-<!--  if (!inputName) return-->
-
-<!--  loading.value = true-->
-<!--  error.value = ''-->
-<!--  target.value = null-->
-
-<!--  try {-->
-<!--    await runTransaction(db, async (transaction) => {-->
-<!--      const usersRef = collection(db, 'users')-->
-
-<!--      const myNameQuery = query(usersRef, where('name', '==', inputName))-->
-<!--      const mySnapshot = await getDocs(myNameQuery)-->
-
-<!--      if (mySnapshot.empty) {-->
-<!--        throw new Error('Вас нет в списке участников! Попросите админа добавить вас.')-->
-<!--      }-->
-
-<!--      const myDoc = mySnapshot.docs[0]-->
-<!--      const myData = myDoc.data() as User-->
-<!--      const myId = myDoc.id-->
-<!--      const whoDrewMe = myData.santaId // ID человека, который уже вытянул меня (если есть)-->
-
-
-<!--      const q = query(usersRef, where('taken', '==', false))-->
-<!--      const snapshot = await getDocs(q)-->
-
-<!--      const myNameFull = inputName.toLowerCase()-->
-<!--      const mySurnameRoot = getNormalizedSurname(inputName)-->
-
-<!--      const candidates: User[] = snapshot.docs-->
-<!--          .map(d => ({ id: d.id, ...d.data() } as User))-->
-<!--          .filter(u => {-->
-<!--            const candidateNameFull = u.name.trim().toLowerCase()-->
-<!--            const candidateSurnameRoot = getNormalizedSurname(u.name)-->
-
-<!--            // Правило 1: Не я сам-->
-<!--            if (candidateNameFull === myNameFull) return false-->
-
-<!--            // Правило 2: Не однофамильцы-->
-<!--            if (candidateSurnameRoot === mySurnameRoot && mySurnameRoot.length > 2) return false-->
-
-<!--            // Правило 3 (АНТИ-КОЛЬЦО):-->
-<!--            if (whoDrewMe && u.id === whoDrewMe) {-->
-
-<!--              return false-->
-<!--            }-->
-
-<!--            return true-->
-<!--          })-->
-
-
-<!--      let finalCandidates = candidates;-->
-
-<!--      if (candidates.length === 0 && whoDrewMe) {-->
-
-<!--        throw new Error('Нет доступных участников! Похоже, вы последний или остались только родственники.')-->
-<!--      }-->
-
-<!--      const randomIndex = Math.floor(Math.random() * finalCandidates.length)-->
-<!--      const selectedUser = finalCandidates[randomIndex]-->
-<!--      const selectedRef = doc(db, 'users', selectedUser.id)-->
-
-<!--      // 3. Проверка race condition-->
-<!--      const freshDoc = await transaction.get(selectedRef)-->
-<!--      if (freshDoc.exists() && freshDoc.data().taken) {-->
-<!--        throw new Error('Эльфы не успели, попробуй снова!')-->
-<!--      }-->
-
-
-<!--      transaction.update(selectedRef, {-->
-<!--        taken: true,-->
-<!--        santaId: myId-->
-<!--      })-->
-
-<!--      target.value = selectedUser.name-->
-<!--    })-->
-<!--  } catch (e: unknown) {-->
-<!--    if (e instanceof Error) error.value = e.message-->
-<!--  } finally {-->
-<!--    loading.value = false-->
-<!--  }-->
-<!--}-->
-<!--</script>-->
-
-<!--<template>-->
-<!--  <div class="card">-->
-<!--    <div class="header-row">-->
-<!--      <img :src="logoImg2" alt="Secret Santa" class="logo" />-->
-<!--      <h1>Secret Santa</h1>-->
-<!--    </div>-->
-
-<!--    <div class="input-group">-->
-<!--      <input-->
-<!--          v-model="name"-->
-<!--          placeholder="Имя"-->
-<!--          :disabled="loading || !!target"-->
-<!--          @keydown.enter="getSanta"-->
-<!--      />-->
-<!--    </div>-->
-
-<!--    <button-->
-<!--        class="xmas-btn"-->
-<!--        @click="getSanta"-->
-<!--        :disabled="!name || loading || !!target"-->
-<!--    >-->
-<!--      {{ loading ? '🎄 Поиск...' : '🎁 Кто мне выпал ?' }}-->
-<!--    </button>-->
-
-<!--    <div v-if="target" class="result fade-in">-->
-<!--      <img :src="logoImg" alt="Secret Santa" class="logo" />-->
-<!--      <p>Ты — Тайный Санта для:</p>-->
-<!--      <h2>{{ target }}</h2>-->
-<!--    </div>-->
-
-<!--    <div v-if="error" class="error shake">-->
-<!--      {{ error }}-->
-<!--    </div>-->
-<!--  </div>-->
-<!--</template>-->
-
-<!--<style scoped>-->
-<!--.card {-->
-<!--  background: rgba(255, 255, 255, 0.35);-->
-<!--  backdrop-filter: blur(7px);-->
-<!--  -webkit-backdrop-filter: blur(7px);-->
-<!--  padding: 1rem;-->
-<!--  border-radius: 20px;-->
-<!--  box-shadow: 0 8px 32px rgba(0, 0, 0, 0.1);-->
-<!--  text-align: center;-->
-<!--  border: 1px solid rgba(255, 255, 255, 0.3);-->
-<!--  max-width: 90%;-->
-<!--  width: 350px;-->
-<!--  margin: 0 auto;-->
-<!--}-->
-
-<!--/* Flex контейнер для лого и текста */-->
-<!--.header-row {-->
-<!--  display: flex;-->
-<!--  align-items: center;-->
-<!--  justify-content: center;-->
-<!--  gap: 15px;-->
-<!--  margin-bottom: 2rem;-->
-<!--}-->
-
-<!--.logo {-->
-<!--  max-width: 120px;-->
-<!--  height: auto;-->
-<!--  filter: drop-shadow(0 2px 4px rgba(0,0,0,0.2));-->
-<!--}-->
-
-<!--h1 {-->
-<!--  color: var(&#45;&#45;green);-->
-<!--  margin: 0;-->
-<!--  text-shadow: 1px 1px 0 rgba(255,255,255,0.5);-->
-<!--  font-weight: 700;-->
-<!--  font-size: 3.2rem;-->
-<!--  line-height: 1;-->
-<!--  text-align: left;-->
-<!--}-->
-
-<!--input {-->
-<!--  width: 100%;-->
-<!--  padding: 15px;-->
-<!--  font-size: 1.1rem;-->
-<!--  background: rgba(255, 255, 255, 0.5);-->
-<!--  border: 2px solid rgba(255, 255, 255, 0.3);-->
-<!--  border-radius: 12px;-->
-<!--  margin-bottom: 1rem;-->
-<!--  box-sizing: border-box;-->
-<!--  outline: none;-->
-<!--  text-align: center;-->
-<!--  color: #000;-->
-<!--  transition: background 0.3s;-->
-<!--}-->
-
-<!--input::placeholder { color: rgba(0, 0, 0, 0.6); }-->
-<!--input:focus { border-color: var(&#45;&#45;green); background: rgba(255, 255, 255, 0.85); }-->
-
-<!--.xmas-btn {-->
-<!--  width: 100%;-->
-<!--  padding: 15px;-->
-<!--  font-size: 1.2rem;-->
-<!--  background: var(&#45;&#45;green);-->
-<!--  color: white;-->
-<!--  border: none;-->
-<!--  border-radius: 12px;-->
-<!--  cursor: pointer;-->
-<!--  transition: transform 0.2s;-->
-<!--  box-shadow: 0 4px 0 #0f4224;-->
-<!--}-->
-
-<!--.xmas-btn:active { transform: translateY(4px); box-shadow: none; }-->
-<!--.xmas-btn:disabled { background: #5a7a66; box-shadow: none; transform: none; opacity: 0.8; cursor: not-allowed; }-->
-
-<!--.result {-->
-<!--  margin-top: 2rem;-->
-<!--  color: var(&#45;&#45;red);-->
-<!--  text-shadow: 0 1px 2px rgba(255,255,255,0.8);-->
-<!--}-->
-
-<!--.result h2 {-->
-<!--  font-size: 3rem;-->
-<!--  margin: 0.5rem 0;-->
-<!--  color: var(&#45;&#45;green);-->
-<!--  font-weight: 800;-->
-<!--  background: rgba(255,255,255,0.4);-->
-<!--  padding: 8px;-->
-<!--  border-radius: 8px;-->
-<!--}-->
-
-<!--.result p {-->
-<!--  font-size: 1.3rem;-->
-<!--  margin: 0.5rem 0;-->
-<!--  color: var(&#45;&#45;red);-->
-<!--  font-weight: 400;-->
-<!--  background: rgba(255,255,255,0.4);-->
-<!--  padding: 8px;-->
-<!--  border-radius: 8px;-->
-<!--}-->
-
-
-
-
-<!--.error {-->
-<!--  margin-top: 1rem;-->
-<!--  color: #fff;-->
-<!--  background: rgba(212, 36, 38, 0.8);-->
-<!--  padding: 10px;-->
-<!--  border-radius: 8px;-->
-<!--  font-weight: bold;-->
-<!--}-->
-
-<!--.fade-in { animation: fadeIn 0.5s ease-out; }-->
-<!--@keyframes fadeIn { from { opacity: 0; transform: translateY(10px); } to { opacity: 1; transform: translateY(0); } }-->
-<!--</style>-->
-
 <script setup lang="ts">
 import { ref } from 'vue'
 import { collection, query, where, getDocs, runTransaction, doc } from 'firebase/firestore'
@@ -305,6 +42,8 @@ const getSanta = async () => {
       }
 
       const myDoc = mySnapshot.docs[0]
+      if (!myDoc) throw new Error('Ошибка получения данных пользователя')
+
       const myData = myDoc.data() as User
       const myId = myDoc.id
       const whoDrewMe = myData.santaId
@@ -333,6 +72,9 @@ const getSanta = async () => {
 
       const randomIndex = Math.floor(Math.random() * candidates.length)
       const selectedUser = candidates[randomIndex]
+
+      if (!selectedUser) throw new Error('Ошибка выбора кандидата')
+
       const selectedRef = doc(db, 'users', selectedUser.id)
 
       const freshDoc = await transaction.get(selectedRef)
@@ -393,15 +135,14 @@ const getSanta = async () => {
 
 <style scoped>
 .card {
-  /* Очень легкое, едва заметное стекло */
   background: rgba(255, 255, 255, 0.15);
   backdrop-filter: blur(6px);
   -webkit-backdrop-filter: blur(6px);
 
-  /* Тончайшая граница */
+
   border: 1px solid rgba(255, 255, 255, 0.15);
 
-  /* Очень мягкая тень, чтобы не было ощущения "кирпича" */
+
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.05);
 
   padding: 1.5rem;
@@ -412,7 +153,7 @@ const getSanta = async () => {
   margin: 0 auto;
 }
 
-/* Шапка */
+
 .header-row {
   display: flex;
   align-items: center;
@@ -422,7 +163,7 @@ const getSanta = async () => {
 }
 
 .logo {
-  max-width: 90px;
+  max-width: 100px;
   height: auto;
   filter: drop-shadow(0 4px 8px rgba(0,0,0,0.1));
 }
@@ -435,20 +176,23 @@ const getSanta = async () => {
 }
 
 h1 {
+  font-family: 'Mountains of Christmas', cursive !important;
   color: var(--green);
   margin: 0;
-  text-shadow: 0 2px 15px rgba(255,255,255,0.8);
-  font-weight: 700;
-  font-size: 3.2rem;
+  text-shadow:
+      0 2px 3px #ffffff,
+      0 4px 10px rgba(255,255,255,0.9),
+      0 0 30px rgba(255,255,255,0.7);
+  font-weight: 700 !important;
+  font-size: 3rem !important;
   line-height: 1;
 }
 
-/* Инпут */
+
 input {
   width: 100%;
   padding: 12px;
   font-size: 1.6rem;
-  /* Полупрозрачный фон для инпута */
   background: rgba(255, 255, 255, 0.5);
   border: 1px solid rgba(255, 255, 255, 0.3);
   border-radius: 16px;
@@ -525,7 +269,6 @@ input:focus {
   box-shadow: 0 4px 10px rgba(0,0,0,0.15);
 }
 
-/* Анимации */
 .fade-in { animation: fadeIn 0.8s cubic-bezier(0.19, 1, 0.22, 1); }
 @keyframes fadeIn {
   from { opacity: 0; transform: translateY(20px) scale(0.95); }
@@ -541,6 +284,3 @@ input:focus {
   100% { transform: translateX(0); }
 }
 </style>
-
-
-
